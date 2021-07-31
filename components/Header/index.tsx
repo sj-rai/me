855138c4b52872d4c3530cabd63965190cb14be4
Sanjoth J Rai <sanjr4279@gmail.com>
2021-08-01 00:16:07 +0530

    font and other changes

---
+++ components/Header/index.tsx
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+import React from 'react';
+import styles from './header.module.css'
+
+export default function Header() {
+    return (
+        <Head>
+            <>
+                <div className={styles.navigation} >
+                    {/*<div id="blog"><a href="#">Blog </a></div>*/}
+                    {/*<div className={"blogs"}><a href="/blogs">Blogs </a></div>*/}
+                    {/*<div className={"notes"}><a href="/notes">Notes </a></div>*/}
+                    <a className={"notes"} href="#">Notes </a>
+                    <a className={"blogs"} href="#">Blogs </a>
+                    <a className={"playlist"} href="#">Playlist </a>
+                    <a className={"rss"} href="#">RSS feed </a>
+                    <a className={"bookmarks"} href="#">Bookmarks </a>
+                </div>
+            </>
+        </Head>
+    );
+}
---
+++ components/Switch/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styles from './switch.module.css';
+
+const Switch = () => {
+    return (
+        <>
+            <input
+                className={styles.reactSwitchCheckbox}
+                id={`react-switch-new`}
+                type="checkbox"
+            />
+            <label
+                className={styles.reactSwitchLabel}
+                htmlFor={`react-switch-new`}
+            >
+                <span className={styles.reactSwitchButton} />
+            </label>
+        </>
+    );
+};
+
+export default Switch;
