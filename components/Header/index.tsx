9b0f16ece67b26d42e80a1ecaf2cb80c800b1293
Sanjoth J Rai <sanjr4279@gmail.com>
2021-08-01 10:28:56 +0530

    prettier

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,22 +1,32 @@
-import Head from 'next/head'
-import React from 'react';
-import styles from './header.module.css'
+import Head from "next/head";
+import React from "react";
+import styles from "./header.module.css";
 
 export default function Header() {
-    return (
-        <Head>
-            <>
-                <div className={styles.navigation} >
-                    {/*<div id="blog"><a href="#">Blog </a></div>*/}
-                    {/*<div className={"blogs"}><a href="/blogs">Blogs </a></div>*/}
-                    {/*<div className={"notes"}><a href="/notes">Notes </a></div>*/}
-                    <a className={"notes"} href="#">Notes 📝</a>
-                    <a className={"blogs"} href="#">Blogs 🖥</a>
-                    <a className={"playlist"} href="#">Playlist 🎵</a>
-                    <a className={"rss"} href="#">RSS feed </a>
-                    <a className={"bookmarks"} href="#">Bookmarks </a>
-                </div>
-            </>
-        </Head>
-    );
+  return (
+    <Head>
+      <>
+        <div className={styles.navigation}>
+          {/*<div id="blog"><a href="#">Blog </a></div>*/}
+          {/*<div className={"blogs"}><a href="/blogs">Blogs </a></div>*/}
+          {/*<div className={"notes"}><a href="/notes">Notes </a></div>*/}
+          <a className={"notes"} href="#">
+            Notes 📝
+          </a>
+          <a className={"blogs"} href="#">
+            Blogs 🖥
+          </a>
+          <a className={"playlist"} href="#">
+            Playlist 🎵
+          </a>
+          <a className={"rss"} href="#">
+            RSS feed{" "}
+          </a>
+          <a className={"bookmarks"} href="#">
+            Bookmarks{" "}
+          </a>
+        </div>
+      </>
+    </Head>
+  );
 }
